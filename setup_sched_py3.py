a6f4350cc8ef9fb66837a9637d9c2f2792d113fb
akirapop <cbaluta20@gmail.com>
2016-09-09 18:46:00 +0900

    Fixed path names for python build tool.

--- setup_sched_py3.py
+++ setup_sched_py3.py
@@ -6,8 +6,8 @@ setup(name="Py_sched",
       version='0.00', 
       ext_modules=[Extension('Py_sched',['C/Py_Sched.c', 'C/orbit_wrappers.c', 'C/cache.c', 'C/memory.c', 
                     'C/misc_at_utilities.c', 'C/attitude_wrappers.c','C/solar_system_wrappers.c'],
-                   include_dirs=['/net/share/adsoft/atFunctions/3.3/include'],
-                   library_dirs=['/net/share/adsoft/atFunctions/3.3/linux','/home/baluta/lib/'],
+                   include_dirs=['/Users/axsh/include'],
+                   library_dirs=['/Users/axsh/ChrisToyProject/pytako/atFunctions/3.3/'],  #,'/home/baluta/lib/'],
                    libraries=['atFunctions', 'cfitsio']
       )]
 )
